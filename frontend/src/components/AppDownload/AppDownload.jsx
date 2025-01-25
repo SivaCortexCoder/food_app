51a2a882b3fd4818747653924fca901e377fcd48
Sivakumar K <sivaofficial76@gmail.com>
2025-01-25 15:10:46 +0530

    Update AppDownload.jsx

--- frontend/src/components/AppDownload/AppDownload.jsx
+++ frontend/src/components/AppDownload/AppDownload.jsx
@@ -8,8 +8,7 @@ const AppDownload = () => {
     <div className='app-download' id='app-download'>
         <p>Todays Special<br/>Limited Quantity only!!</p>
         <div className="app-download-platforms">
-            <img src={assets.mutton_biryani} alt="" />
-            <img src={assets.prawn_biryani} alt="" />
+           
         </div>
       
     </div>
